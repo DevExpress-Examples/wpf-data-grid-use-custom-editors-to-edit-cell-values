bbc07d07f3dd05013cdd71d2d8de60bd8785e8d2
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXGrid_CustomEditors/Window1.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Markup;
+
+namespace DXGrid_CustomEditors {
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+            grid.ItemsSource = Products.GetData();
+        }
+
+        private void grid_CustomUnboundColumnData(object sender, DevExpress.Xpf.Grid.GridColumnDataEventArgs e) {
+            if (e.Column.FieldName == "Total") {
+                e.Value = Convert.ToInt32(grid.GetCellValue(grid.GetRowHandleByListIndex(e.ListSourceRowIndex), "UnitPrice")) *
+                    Convert.ToDouble(grid.GetCellValue(grid.GetRowHandleByListIndex(e.ListSourceRowIndex), "UnitsOnOrder"));
+            }
+        }
+    }
+
+    public class Products {
+        public static List<Product> GetData() {
+            List<Product> data = new List<Product>();
+            data.Add(new Product() { ProductName = "Chai", UnitPrice = 18, UnitsOnOrder = 10 });
+            data.Add(new Product() { ProductName = "Ipoh Coffee", UnitPrice=36.8, UnitsOnOrder = 12 });
+            data.Add(new Product() { ProductName = "Outback Lager", UnitPrice=12, UnitsOnOrder = 25 });
+            data.Add(new Product() { ProductName = "Boston Crab Meat", UnitPrice=18.4, UnitsOnOrder = 18 });
+            data.Add(new Product() { ProductName = "Konbu", UnitPrice=6, UnitsOnOrder = 24 });
+            return data;
+        }
+    }
+    public class Product {
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitsOnOrder { get; set; }
+    }
+
+    public class IntToDoubleConverter : MarkupExtension, IValueConverter {
+        public override object ProvideValue(IServiceProvider serviceProvider) {
+            return this;
+        }
+        object IValueConverter.Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            return Convert.ToDouble(value);
+        }
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            return Convert.ToInt32(value);
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to Use Custom Editors to Edit Cell Values
+
+
+<p>This example shows how to use custom editors (ProgressBar and Slider) to display and edit the 'Units On Order' column's values. Custom editors are defined via templates. The column's DisplayTemplate property defines a template that displays column values. The EditTemplate property defines a template that represents an editor used to edit cell values.</p>
+
+<br/>
+
+
