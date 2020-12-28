f11a1d99823712e38c53760e2c2b9bcf93e29370
DevExpressExampleBot <fakeEmail>
2020-12-28 23:04:24 +0300

    Source auto update [skip ci]

---
+++ VB/ViewModel.vb
@@ -0,0 +1,83 @@
+﻿Imports DevExpress.Mvvm
+Imports System.Collections.ObjectModel
+
+Namespace DXGrid_CustomEditors
+	Public Class DemoViewModel
+		Inherits ViewModelBase
+
+		Private _products As ObservableCollection(Of Product)
+		Public Sub New()
+			Products = GenerateData()
+		End Sub
+		Private Function GenerateData() As ObservableCollection(Of Product)
+			Dim data As New ObservableCollection(Of Product)()
+			data.Add(New Product() With {
+				.ProductName = "Chai",
+				.UnitPrice = 18,
+				.UnitsOnOrder = 10
+			})
+			data.Add(New Product() With {
+				.ProductName = "Ipoh Coffee",
+				.UnitPrice = 36.8,
+				.UnitsOnOrder = 12
+			})
+			data.Add(New Product() With {
+				.ProductName = "Outback Lager",
+				.UnitPrice = 12,
+				.UnitsOnOrder = 25
+			})
+			data.Add(New Product() With {
+				.ProductName = "Boston Crab Meat",
+				.UnitPrice = 18.4,
+				.UnitsOnOrder = 18
+			})
+			data.Add(New Product() With {
+				.ProductName = "Konbu",
+				.UnitPrice = 6,
+				.UnitsOnOrder = 24
+			})
+			Return data
+		End Function
+		Public Property Products() As ObservableCollection(Of Product)
+			Get
+				Return _products
+			End Get
+			Set(ByVal value As ObservableCollection(Of Product))
+				If value Is _products Then
+					Return
+				End If
+				_products = value
+				Me.RaisePropertyChanged("Products")
+			End Set
+		End Property
+	End Class
+
+	Public Class Product
+		Inherits BindableBase
+
+		Public Property ProductName() As String
+			Get
+				Return GetProperty(Of String)(Function() ProductName)
+			End Get
+			Set(ByVal value As String)
+				SetProperty(Function() ProductName, value)
+			End Set
+		End Property
+		Public Property UnitPrice() As Double
+			Get
+				Return GetProperty(Of Double)(Function() UnitPrice)
+			End Get
+			Set(ByVal value As Double)
+				SetProperty(Function() UnitPrice, value)
+			End Set
+		End Property
+		Public Property UnitsOnOrder() As Integer
+			Get
+				Return GetProperty(Of Integer)(Function() UnitsOnOrder)
+			End Get
+			Set(ByVal value As Integer)
+				SetProperty(Function() UnitsOnOrder, value)
+			End Set
+		End Property
+	End Class
+End Namespace
---
+++ VB/Window1.xaml.vb
@@ -0,0 +1,42 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports System.Windows.Markup
+
+Namespace DXGrid_CustomEditors
+	Partial Public Class Window1
+		Inherits Window
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+
+	Public Class IntToDoubleConverter
+		Inherits MarkupExtension
+		Implements IValueConverter
+
+		Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
+			Return Me
+		End Function
+		Private Function IValueConverter_Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.Convert
+			If TypeOf value Is Integer Then
+				Return Convert.ToDouble(value)
+			End If
+			Return Double.NaN
+		End Function
+		Private Function IValueConverter_ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
+			Return Convert.ToInt32(value)
+		End Function
+	End Class
+End Namespace
