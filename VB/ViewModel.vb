bbd812af2984ca3a0c6b85937ff3057ac876a0bc
DevExpressExampleBot <fakeEmail>
2021-10-04 14:14:57 +0300

    Source auto update [skip ci]

--- VB/ViewModel.vb
+++ VB/ViewModel.vb
@@ -1,83 +1,66 @@
-﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm
+Imports System.Collections.Generic
 Imports System.Collections.ObjectModel
 
 Namespace DXGrid_CustomEditors
-	Public Class DemoViewModel
-		Inherits ViewModelBase
 
-		Private _products As ObservableCollection(Of Product)
-		Public Sub New()
-			Products = GenerateData()
-		End Sub
-		Private Function GenerateData() As ObservableCollection(Of Product)
-			Dim data As New ObservableCollection(Of Product)()
-			data.Add(New Product() With {
-				.ProductName = "Chai",
-				.UnitPrice = 18,
-				.UnitsOnOrder = 10
-			})
-			data.Add(New Product() With {
-				.ProductName = "Ipoh Coffee",
-				.UnitPrice = 36.8,
-				.UnitsOnOrder = 12
-			})
-			data.Add(New Product() With {
-				.ProductName = "Outback Lager",
-				.UnitPrice = 12,
-				.UnitsOnOrder = 25
-			})
-			data.Add(New Product() With {
-				.ProductName = "Boston Crab Meat",
-				.UnitPrice = 18.4,
-				.UnitsOnOrder = 18
-			})
-			data.Add(New Product() With {
-				.ProductName = "Konbu",
-				.UnitPrice = 6,
-				.UnitsOnOrder = 24
-			})
-			Return data
-		End Function
-		Public Property Products() As ObservableCollection(Of Product)
-			Get
-				Return _products
-			End Get
-			Set(ByVal value As ObservableCollection(Of Product))
-				If value Is _products Then
-					Return
-				End If
-				_products = value
-				Me.RaisePropertyChanged("Products")
-			End Set
-		End Property
-	End Class
+    Public Class DemoViewModel
+        Inherits ViewModelBase
 
-	Public Class Product
-		Inherits BindableBase
+        Public Sub New()
+            Products = New ObservableCollection(Of Product)(GenerateData())
+        End Sub
 
-		Public Property ProductName() As String
-			Get
-				Return GetProperty(Of String)(Function() ProductName)
-			End Get
-			Set(ByVal value As String)
-				SetProperty(Function() ProductName, value)
-			End Set
-		End Property
-		Public Property UnitPrice() As Double
-			Get
-				Return GetProperty(Of Double)(Function() UnitPrice)
-			End Get
-			Set(ByVal value As Double)
-				SetProperty(Function() UnitPrice, value)
-			End Set
-		End Property
-		Public Property UnitsOnOrder() As Integer
-			Get
-				Return GetProperty(Of Integer)(Function() UnitsOnOrder)
-			End Get
-			Set(ByVal value As Integer)
-				SetProperty(Function() UnitsOnOrder, value)
-			End Set
-		End Property
-	End Class
+        Private Shared Iterator Function GenerateData() As IEnumerable(Of Product)
+            Yield New Product() With {.ProductName = "Chai", .UnitPrice = 18, .UnitsOnOrder = 10}
+            Yield New Product() With {.ProductName = "Ipoh Coffee", .UnitPrice = 36.8, .UnitsOnOrder = 12}
+            Yield New Product() With {.ProductName = "Outback Lager", .UnitPrice = 12, .UnitsOnOrder = 25}
+            Yield New Product() With {.ProductName = "Boston Crab Meat", .UnitPrice = 18.4, .UnitsOnOrder = 18}
+            Yield New Product() With {.ProductName = "Konbu", .UnitPrice = 6, .UnitsOnOrder = 24}
+        End Function
+
+        Public Property Products As ObservableCollection(Of Product)
+            Get
+                Return GetValue(Of ObservableCollection(Of Product))(NameOf(DemoViewModel.Products))
+            End Get
+
+            Set(ByVal value As ObservableCollection(Of Product))
+                SetValue(value, NameOf(DemoViewModel.Products))
+            End Set
+        End Property
+    End Class
+
+    Public Class Product
+        Inherits BindableBase
+
+        Public Property ProductName As String
+            Get
+                Return GetValue(Of String)(NameOf(Product.ProductName))
+            End Get
+
+            Set(ByVal value As String)
+                SetValue(value, NameOf(Product.ProductName))
+            End Set
+        End Property
+
+        Public Property UnitPrice As Double
+            Get
+                Return GetValue(Of Double)(NameOf(Product.UnitPrice))
+            End Get
+
+            Set(ByVal value As Double)
+                SetValue(value, NameOf(Product.UnitPrice))
+            End Set
+        End Property
+
+        Public Property UnitsOnOrder As Integer
+            Get
+                Return GetValue(Of Integer)(NameOf(Product.UnitsOnOrder))
+            End Get
+
+            Set(ByVal value As Integer)
+                SetValue(value, NameOf(Product.UnitsOnOrder))
+            End Set
+        End Property
+    End Class
 End Namespace
--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,42 +1,34 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
+Imports System
 Imports System.Windows
 Imports System.Windows.Controls
 Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 Imports System.Windows.Markup
 
 Namespace DXGrid_CustomEditors
-	Partial Public Class Window1
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    Public Partial Class Window1
+        Inherits Window
 
-	Public Class IntToDoubleConverter
-		Inherits MarkupExtension
-		Implements IValueConverter
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
 
-		Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
-			Return Me
-		End Function
-		Private Function IValueConverter_Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.Convert
-			If TypeOf value Is Integer Then
-				Return Convert.ToDouble(value)
-			End If
-			Return Double.NaN
-		End Function
-		Private Function IValueConverter_ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
-			Return Convert.ToInt32(value)
-		End Function
-	End Class
+    Public Class IntToDoubleConverter
+        Inherits MarkupExtension
+        Implements IValueConverter
+
+        Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
+            Return Me
+        End Function
+
+        Private Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As Globalization.CultureInfo) As Object Implements IValueConverter.Convert
+            If TypeOf value Is Integer Then Return System.Convert.ToDouble(value)
+            Return Double.NaN
+        End Function
+
+        Private Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
+            Return System.Convert.ToInt32(value)
+        End Function
+    End Class
 End Namespace
