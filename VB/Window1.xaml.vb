7cd66fed42e2d9d3281fccca13ab51c3b1fa06b3
DevExpressExampleBot <fakeEmail>
2023-02-17 16:18:04 +0400

    Source auto update [skip ci]

--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,43 +1,39 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
 Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 Imports System.Windows.Markup
 
 Namespace DXGrid_CustomEditors
-	Partial Public Class Window1
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-		Private Sub grid_CustomUnboundColumnData(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.GridColumnDataEventArgs)
-			If e.Column.FieldName = "Total" Then
-				e.Value = Convert.ToInt32(grid.GetCellValueByListIndex(e.ListSourceRowIndex, "UnitPrice")) * Convert.ToDouble(grid.GetCellValueByListIndex(e.ListSourceRowIndex, "UnitsOnOrder"))
-			End If
-		End Sub
-	End Class
 
-	Public Class IntToDoubleConverter
-		Inherits MarkupExtension
-		Implements IValueConverter
-		Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
-			Return Me
-		End Function
-		Private Function IValueConverter_Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.Convert
-			Return Convert.ToDouble(value)
-		End Function
-		Private Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
-			Return Convert.ToInt32(value)
-		End Function
-	End Class
+    Public Partial Class Window1
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+
+        Private Sub grid_CustomUnboundColumnData(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.GridColumnDataEventArgs)
+            If Equals(e.Column.FieldName, "Total") Then
+                e.Value = Convert.ToInt32(Me.grid.GetCellValueByListIndex(e.ListSourceRowIndex, "UnitPrice")) * Convert.ToDouble(Me.grid.GetCellValueByListIndex(e.ListSourceRowIndex, "UnitsOnOrder"))
+            End If
+        End Sub
+    End Class
+
+    Public Class IntToDoubleConverter
+        Inherits MarkupExtension
+        Implements IValueConverter
+
+        Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
+            Return Me
+        End Function
+
+        Private Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As Globalization.CultureInfo) As Object Implements IValueConverter.Convert
+            Return System.Convert.ToDouble(value)
+        End Function
+
+        Private Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
+            Return System.Convert.ToInt32(value)
+        End Function
+    End Class
 End Namespace
